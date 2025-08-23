733c3394e853d66cfcab8e1df412d53c5cc28bd2
codewithjaydev <jaydevthapa8@gmail.com>
2025-08-23 20:08:51 +0545

    django project

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import home_page_view
-# from .views import about_page
+from .views import home_page_view,signin_view,signup_view
+
 urlpatterns=[
-    path("",home_page_view),    
+    path("",home_page_view, name='home'),    
+    path('signin/',signin_view,name="signin"),
+    path('signup/', signup_view, name='signup'),
+    # path('booking/',booking_view, name='booking'),
 ]
